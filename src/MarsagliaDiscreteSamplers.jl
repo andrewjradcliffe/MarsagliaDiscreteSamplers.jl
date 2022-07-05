be98a7b37f95bdc6b1aa9f40b1f6457b335825a2
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-07-05 15:20:05 -0700

    Basic deps

--- src/MarsagliaDiscreteSamplers.jl
+++ src/MarsagliaDiscreteSamplers.jl
@@ -1,5 +1,11 @@
 module MarsagliaDiscreteSamplers
 
-# Write your package code here.
+using Random, Test, BenchmarkTools
+using LoopVectorization
+using VectorizedReduction
+
+export sqhist!, sqhist
+
+include("squarehistogram.jl")
 
 end
