178433e451daa22e8336761f202653a034659b47
Andrew Radcliffe <andrewjradcliffe@gmail.com>
2022-07-05 18:24:12 -0700

    Optimized generation case

--- src/generation.jl
+++ src/generation.jl
@@ -47,6 +47,8 @@ generate(K::Vector{Int}, V::Vector{T}, dims::Vararg{Int, N}) where {T<:AbstractF
 ################
 # Why bother with @turbo? Well, unless one can guarantee that @simd ivdep is safe,
 # @turbo will exhibit superior performance.
+vgenerate(K::Vector{Int}, V::Vector{<:AbstractFloat}) = generate(K, V)
+
 function vgenerate!(A::AbstractArray{<:Integer}, u::AbstractArray{Float64}, K::Vector{Int}, V::Vector{T}) where {T<:AbstractFloat}
     n = length(K)
     checkbounds(V, n)
@@ -63,3 +65,51 @@ vgenerate!(A::AbstractArray{<:Integer}, K::Vector{Int}, V::Vector{T}) where {T<:
 
 vgenerate(K::Vector{Int}, V::Vector{T}, dims::Vararg{Int, N}) where {T<:AbstractFloat} where {N} =
     vgenerate!(Array{Int}(undef, dims), K, V)
+
+
+################
+# Equal probability case admits an optimized form:
+# U ~ Uniform(0,1)
+# j = ⌊nU + 1⌋; return j
+
+function generate(n::Int)
+    n > 0 || throw(ArgumentError("n must be > 0"))
+    unsafe_trunc(Int, rand() * n) + 1
+end
+
+function generate!(A::AbstractArray{<:Integer}, u::AbstractArray{Float64}, n::Int)
+    n > 0 || throw(ArgumentError("n must be > 0"))
+    n == 1 && return fill!(A, 1)
+    rand!(u)
+    @inbounds for i ∈ eachindex(A, u)
+        A[i] = unsafe_trunc(Int, u[i] * n) + 1
+    end
+    A
+end
+generate!(A::AbstractArray, n::Int) = generate!(A, similar(A, Float64), n)
+
+generate(n::Int, dims::NTuple{N, Int}) where {N} = generate!(Array{Int}(undef, dims), n)
+generate(n::Int, dims::Vararg{Int, N}) where {N} = generate(n, dims)
+
+################
+vgenerate(n::Int) = generate(n)
+function vfill!(A::AbstractArray, v::Real)
+    @turbo for i ∈ eachindex(A)
+        A[i] = v
+    end
+    A
+end
+
+function vgenerate!(A::AbstractArray{<:Integer}, u::AbstractArray{Float64}, n::Int)
+    n > 0 || throw(ArgumentError("n must be > 0"))
+    n == 1 && return vfill!(A, 1)
+    rand!(u)
+    @turbo for i ∈ eachindex(A, u)
+        A[i] = unsafe_trunc(Int, u[i] * n) + 1
+    end
+    A
+end
+vgenerate!(A::AbstractArray, n::Int) = vgenerate!(A, similar(A, Float64), n)
+
+vgenerate(n::Int, dims::NTuple{N, Int}) where {N} = vgenerate!(Array{Int}(undef, dims), n)
+vgenerate(n::Int, dims::Vararg{Int, N}) where {N} = vgenerate(n, dims)
